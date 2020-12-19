b93ef8db5b3ad2a53fbc8a05a92e064b51e68ec7
Gianni Mondello <gianni.mondello@live.it>
2020-12-19 12:22:49 +0100

    text ransitions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,42 @@
 <template>
   
-    <kinesis-container class=" h-screen bg-header text-header flex flex-col justify-center items-center">
-        
-          <kinesis-element class="title" :strength="20">
+    <kinesis-container class=" h-screen bg-header text-header flex flex-col justify-center items-center ">
+        <slide-x-left-transition :duration="2000"   >
+          <kinesis-element v-show="show"  class="title inline-block" :strength="20">
             Gianni Mondello
           </kinesis-element>
-          <kinesis-element class="sub-title" :strength="30">
+        </slide-x-left-transition>
+        <zoom-center-transition  :duration="600">
+          <kinesis-element v-show="show" class="sub-title" :strength="30">
             Web Developer
           </kinesis-element>
+        </zoom-center-transition>
       </kinesis-container>
   
 </template>
 
 <script>
 import { KinesisContainer, KinesisElement} from 'vue-kinesis'
+import { SlideXLeftTransition } from 'vue2-transitions'
+import { ZoomCenterTransition } from 'vue2-transitions'
 export default {
  name: 'Header',
  components: {
    KinesisContainer,
-   KinesisElement
+   KinesisElement,
+   SlideXLeftTransition,
+   ZoomCenterTransition
+
+ },
+ data(){
+   return {
+     show: false
+   }
+ },
+ mounted(){
+   this.show = true
  }
+ 
 
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import Transitions from 'vue2-transitions'
+Vue.use(Transitions)
+
 
 Vue.config.productionTip = false
 

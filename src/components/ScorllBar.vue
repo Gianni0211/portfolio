bd1a9658c4224191748ac1a128b7310d564ba6b5
Gianni Mondello <gianni.mondello@live.it>
2021-01-03 12:52:06 +0100

    scrollbar effect half

--- src/components/ScorllBar.vue
+++ src/components/ScorllBar.vue
@@ -2,10 +2,12 @@
   <div @mousewheel.prevent="handelScroll" class="float-right bg-bar flex justify-center border-bar">
    <ul class="icon-color flex flex-col justify-around my-36 px-1 ">
     <router-link :to="{name: 'home'}" tag="li">
-     <i class="fas fa-home"></i>
+     <i @mouseover="showHome" v-if="!homeOn" class="fas fa-home"></i>
+     <p @mouseleave="closeHome" v-else>Home</p>
     </router-link>
-    <router-link :to="{name: 'about'}" >
-     <i class="far fa-address-card"></i>
+    <router-link    :to="{name: 'about'}" >
+     <i  @mouseover="showAbout" v-if="!aboutOn" class="far fa-address-card"></i>
+     <p  @mouseleave="closeAbout" v-else>About</p>
     </router-link>
     <router-link :to="{name: 'projects'}" tag="li">
      <i class="fas fa-project-diagram"></i>
@@ -22,10 +24,26 @@ export default {
  name: 'ScrollBar',
  data(){
   return{
-   
+   homeOn: false,
+   aboutOn : false
   }
  },
- 
+ methods: {
+   showHome(){
+    this.homeOn = true
+    
+   },
+   showAbout(){
+     this.aboutOn = true
+   },
+   closeHome(){
+     this.homeOn = false
+     
+   },
+   closeAbout(){
+     this.aboutOn = false
+   }
+ }
    
   }
  
@@ -39,7 +57,7 @@ export default {
 }
 .icon-color{
  color: var(--accent-color);
- padding: 3px;
+ padding: 6px;
  transition: 1s;
  
 }

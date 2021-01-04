d8954b062b8c15dc611a746756df5aada455e5f8
Gianni Mondello <gianni.mondello@live.it>
2021-01-04 12:20:55 +0100

    fix about par and pre ptsjs installation

--- src/components/AboutPar.vue
+++ src/components/AboutPar.vue
@@ -5,13 +5,13 @@
    <p class="par">
     Sono un sviluppatore web appassionato di tecnologia, con buone capacità di comunicazione e del lavoro in team, con conoscenze di sviluppo web e programmazione.
     </p>
-    <p>
+    <p class="par">
 Ho maturato competenze nel utilizzo della metodologia agile SCRUM.
 </p>
-<p>
+<p class="par">
 Sono molto curioso e con un ottima attitudine nell'imparare costantemente nuove tecnologie per poter crescere come sviluppatore.
 </p>
-<p>
+<p class="par">
 Le mie caratteristiche fondamentali sono la serietà, la passione e la voglia di migliorare e mettermi in gioco.
    </p>
   

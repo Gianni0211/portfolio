6da222b3b74ececbb1b0ab98a59e2e921f0de524
Gianni Mondello <gianni.mondello@live.it>
2021-01-04 11:55:09 +0100

    scrollbar finished with transition on icons

--- src/components/ScorllBar.vue
+++ src/components/ScorllBar.vue
@@ -1,19 +1,51 @@
 <template>
   <div @mousewheel.prevent="handelScroll" class="float-right bg-bar flex justify-center border-bar">
-   <ul class="icon-color flex flex-col justify-around my-36 px-1 ">
-    <router-link :to="{name: 'home'}" tag="li">
-     <i @mouseover="showHome" v-if="!homeOn" class="fas fa-home"></i>
-     <p @mouseleave="closeHome" v-else>Home</p>
+   <ul class=" flex flex-col justify-around my-36 px-1 ">
+    <router-link 
+    :to="{name: 'home'}" 
+    
+    tag="li"
+    >
+    <div
+    class=" flex flex-col items-center justify-center" 
+    @mouseover="showHome" 
+    @mouseleave="closeHome" 
+    >
+     <i   id="homeIcon" class="fas fa-home icon "></i>
+     <p  id="homeTitle" class="nav-title" >Home</p>
+
+    </div>
     </router-link>
     <router-link    :to="{name: 'about'}" >
-     <i  @mouseover="showAbout" v-if="!aboutOn" class="far fa-address-card"></i>
-     <p  @mouseleave="closeAbout" v-else>About</p>
+      <div
+    class=" flex flex-col items-center justify-center" 
+    @mouseover="showAbout" 
+    @mouseleave="closeAbout" 
+    >
+     <i id="aboutIcon"  class="far fa-address-card icon"></i>
+     <p id="aboutTitle" class="nav-title">About</p>
+      </div>
     </router-link>
+    
     <router-link :to="{name: 'projects'}" tag="li">
-     <i class="fas fa-project-diagram"></i>
+      <div
+    class=" flex flex-col items-center justify-center" 
+    @mouseover="showProjects" 
+    @mouseleave="closeProjects" 
+    >
+     <i id="projectIcon" class="fas fa-project-diagram icon"></i>
+     <p id="projectTitle" class="nav-title">Progetti</p>
+      </div>
     </router-link>
     <router-link :to="{name: 'contact'}" tag="li">
-     <i class="fas fa-file-contract"></i>
+      <div
+    class=" flex flex-col items-center justify-center" 
+    @mouseover="showContact" 
+    @mouseleave="closeContact" 
+    >
+     <i id="contactIcon" class="fas fa-file-contract icon"></i>
+     <p id="contactTitle" class="nav-title">Contatti</p>
+      </div>
     </router-link>
    </ul>
   </div>
@@ -22,26 +54,59 @@
 <script>
 export default {
  name: 'ScrollBar',
- data(){
-  return{
-   homeOn: false,
-   aboutOn : false
-  }
- },
+ 
  methods: {
    showHome(){
-    this.homeOn = true
+    let homeIcon = document.getElementById('homeIcon');
+    let homeTitle = document.getElementById('homeTitle'); 
+    homeIcon.classList.add('icon-out');
+     
+    homeTitle.classList.add('title-in');
     
    },
    showAbout(){
-     this.aboutOn = true
+    let aboutIcon = document.getElementById('aboutIcon');
+    let aboutTitle = document.getElementById('aboutTitle');
+    aboutIcon.classList.add('icon-out');
+    aboutTitle.classList.add('title-in');
+   },
+   showProjects(){
+    let projectIcon = document.getElementById('projectIcon');
+    let projectTitle = document.getElementById('projectTitle');
+    projectIcon.classList.add('icon-out');
+    projectTitle.classList.add('title-in');
+   },
+   showContact(){
+    let contactIcon = document.getElementById('contactIcon');
+    let contactTitle = document.getElementById('contactTitle');
+    contactIcon.classList.add('icon-out');
+    contactTitle.classList.add('title-in');
    },
    closeHome(){
-     this.homeOn = false
+    let homeIcon = document.getElementById('homeIcon');
+    let homeTitle = document.getElementById('homeTitle'); 
+    homeIcon.classList.remove('icon-out');
+     
+    homeTitle.classList.remove('title-in');
      
    },
    closeAbout(){
-     this.aboutOn = false
+    let aboutIcon = document.getElementById('aboutIcon');
+    let aboutTitle = document.getElementById('aboutTitle');
+    aboutIcon.classList.remove('icon-out');
+    aboutTitle.classList.remove('title-in');
+   },
+   closeProjects(){
+    let projectIcon = document.getElementById('projectIcon');
+    let projectTitle = document.getElementById('projectTitle');
+    projectIcon.classList.remove('icon-out');
+    projectTitle.classList.remove('title-in');
+   },
+   closeContact(){
+    let contactIcon = document.getElementById('contactIcon');
+    let contactTitle = document.getElementById('contactTitle');
+    contactIcon.classList.remove('icon-out');
+    contactTitle.classList.remove('title-in');
    }
  }
    
@@ -51,26 +116,44 @@ export default {
 </script>
 
 <style scoped>
+.icon{
+  position: relative;
+  top: 10px;
+  transition: .5s;
+  opacity: 1;
+  cursor: pointer;
+  color: var(--accent-color);
+  padding: 1px;
+}
+.icon-out{
+  opacity: 0;
+}
+
+.nav-title{
+  position: relative;
+  top: -10px;
+  transition: .5s;
+  opacity: 0;
+  font-size: 75%;
+  font-family: 'Lora', serif;
+  color: var(--accent-color);
+  cursor: pointer;
+  padding: 1px;
+}
+.title-in{
+  opacity: 1;
+}
+
 .bg-bar{
  background-color: var(--third-color);
  height: 100vh;
 }
-.icon-color{
- color: var(--accent-color);
- padding: 6px;
- transition: 1s;
- 
-}
+
 .border-bar{
  border-left: 3px solid var(--accent-color);
  
 }
 
-li:hover{
-  color: white;
-  background-color: var(--accent-color);
-  border-radius: 50%;
-  
-}
+
 
 </style>
